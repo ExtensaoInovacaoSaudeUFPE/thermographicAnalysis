0a3bd09c7568fd9262649b340cc566ffa135ffc9
automaniac <igor.mauricio.eng@gmail.com>
2023-12-29 04:48:26 -0300

    Fix: relative path now works for images and exiftool executable

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
 from src.gui.ThermalComparisonGraph import ThermalComparisonGraph
 from src.infra.FlirImageFactory import FlirImageFactory
-
+import os
 
 def main():
-    imagePath = r'C:\\Users\\igor_\\Documents\\dev\\ufpe\\extensaoColaborativa\\Novas_imagens\\FLIR0686.jpg'
+
+    imagePath = r'test\images\FLIR0676.jpg'
     rgbImage = FlirImageFactory.getRGBImageFromPath(imagePath)
     thermalImage = FlirImageFactory.getThermalImageFromPath(imagePath)
     ThermalComparisonGraph(rgbImage, thermalImage).plot()
 
+def setupPath():
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    root_dir = os.path.dirname(script_dir)
+    os.chdir(root_dir)
+    print("Current working directory:", os.getcwd())
 
 if __name__ == "__main__":
+    setupPath()
     main()
